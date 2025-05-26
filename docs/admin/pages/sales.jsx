b98d1792940aeb452a8b8055fc09a4d169373306
Ruth <146713899+Ruthneemu@users.noreply.github.com>
2025-05-26 16:43:26 +0300

    everything

--- docs/admin/pages/sales.jsx
+++ docs/admin/pages/sales.jsx
@@ -88,7 +88,7 @@ const Sales = () => {
         item_id: item.id,
         item_name: item.name,
         quantity: qty,
-        price: item.sellingPrice,
+        price: item.price,
         total,
       },
     ]);
