089c1107c96b00c684c6a2963ec2495883e221bd
Ruth <146713899+Ruthneemu@users.noreply.github.com>
2025-05-27 14:02:03 +0300

    everything

--- docs/admin/pages/settings.jsx
+++ docs/admin/pages/settings.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '../supabaseClient';
+import  supabase  from '../supabaseClient';
 import Layout from "../components/layout";
 
 export default function Settings() {
