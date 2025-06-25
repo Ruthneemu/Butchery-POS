6b9d622939a9679e050895e895b111dadd3367b7
Ruth <146713899+Ruthneemu@users.noreply.github.com>
2025-06-25 09:19:48 +0000

    everything

--- docs/admin/vite.config.js
+++ docs/admin/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      external: ['prop-types'], // Explicitly mark prop-types as external
+    },
   },
   plugins: [react()],
-})
+});
