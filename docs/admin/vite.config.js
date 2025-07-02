44f4633cce87f2551604c808322b75cd5bfefffd
Ruth <146713899+Ruthneemu@users.noreply.github.com>
2025-07-02 06:44:59 +0000

    everything

--- docs/admin/vite.config.js
+++ docs/admin/vite.config.js
@@ -1,16 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: __dirname,
   base: './',
   build: {
     outDir: 'dist',
-    // Remove all external/globals configuration
+    commonjsOptions: {
+      include: [/prop-types/, /node_modules/]
+    }
   },
   plugins: [react()],
   optimizeDeps: {
-    include: ['prop-types', 'react-csv'] // Ensure these are pre-bundled
+    include: ['prop-types']
   }
 });
