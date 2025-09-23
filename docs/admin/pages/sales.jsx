0ed87cbe0e393d77bac8cbc8442efacbd08fa482
ruthneemu <ruthneemu@gmail.com>
2025-09-23 10:27:13 +0000

    all

--- docs/admin/pages/sales.jsx
+++ docs/admin/pages/sales.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import supabase from '../supabaseClient';
 import Layout from "../components/layout";
+
 const Sales = () => {
 // State for order details
 const [customerName, setCustomerName] = useState('');
